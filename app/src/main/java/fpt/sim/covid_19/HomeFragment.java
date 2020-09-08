bfe4029e782f351eddea7f618080fab827d88d5f
Sabbariabdellah <sabbariabdellah@gmail.com>
2020-09-08 20:16:10 +0100

    home2

--- app/src/main/java/fpt/sim/covid_19/HomeFragment.java
+++ app/src/main/java/fpt/sim/covid_19/HomeFragment.java
@@ -3,15 +3,38 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.Spinner;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class HomeFragment extends Fragment {
+    ImageView flags;
+    Spinner spinner;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home,container,false);
+        View view = inflater.inflate(R.layout.fragment_home,container,false);
+        spinner = view.findViewById(R.id.spinner);
+        flags = view.findViewById(R.id.flag);
+        spinner.setAdapter(new ArrayAdapter<String>(getActivity(),android.R.layout.simple_spinner_item,countryData.countryNames));
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                flags.setImageResource(countryData.countryFlags[spinner.getSelectedItemPosition()]);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+        return view;
     }
+
 }
---
+++ app/src/main/java/fpt/sim/covid_19/countryData.java
@@ -0,0 +1,11 @@
+package fpt.sim.covid_19;
+
+public class countryData {
+    public static final String[] countryNames = {
+        "Mar","Fra","Eng"
+    };
+
+    public static final int[] countryFlags = {
+            R.drawable.morocco, R.drawable.france, R.drawable.united_kingdom
+    };
+}
